fb5704f50000c211651bade67473b6c1335975f9
MahsaSetoode <69714104+MahsaSetoode@users.noreply.github.com>
2023-11-14 02:49:58 +0330

    Update settings.py

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'tasks.middleware.Error404Middleware',
 ]
 
 INTERNAL_IPS = [
