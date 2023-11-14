8c629b2fe215bfd0b5d2f523c103c691ab121d54
MahsaSetoode <69714104+MahsaSetoode@users.noreply.github.com>
2023-11-14 12:18:01 +0330

    change url

--- taskmanager/tasks/urls.py
+++ taskmanager/tasks/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 
-# app_name='tasks'
+
 urlpatterns = [
     path('', views.TaskList.as_view(), name='task_list_create'),
-    path('details/<int:pk>/', views.TaskDetail.as_view(), name='task_details'),
+    path('detail/<int:pk>/', views.TaskDetail.as_view(), name='task_details'),
     path('edit/<int:pk>/', views.TaskEdit.as_view(), name='task_edit'),
     path('delete/<int:pk>/', views.TaskDelete.as_view(), name='task_delete'),
 ]
