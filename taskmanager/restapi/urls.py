4a9fac55e57c7e439c4e8f21220148c679022436
MahsaSetoode <69714104+MahsaSetoode@users.noreply.github.com>
2023-11-14 12:07:29 +0330

    change api url

--- taskmanager/restapi/urls.py
+++ taskmanager/restapi/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     # api endpoints
-    path('', views.TaskListCreateApi.as_view(), name='task_list_create_api'),
-    path('details/<int:pk>/', views.TaskDetailApi.as_view(), name='task_details_api'),
+    path('tasks/', views.TaskListCreateApi.as_view(), name='task_list_create_api'),
+    path('task/<int:pk>/', views.TaskDetailApi.as_view(), name='task_detail_api'),
 ]
